8487ff4d7b4662089f508e81b0ee15a6627bd48a
juan gutierrez <juan.gutierrez20@usa.edu.co>
2026-03-02 07:53:21 -0500

    bucle while

--- taller_1044218091.py
+++ taller_1044218091.py
@@ -9,6 +9,14 @@ else:
 for i in range (5):
     print("vete de acá")
 
+ #bucle while
+
+contador = 1
+while contador <= 5:
+    print("numero: "  + str(contador))
+    contador = contador + 1
+
+
 
 
 
