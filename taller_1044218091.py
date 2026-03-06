43cda33f3372e94531125bdaa07e0ccecc997be6
Juan Gutierrez <juan.gutierrez20@usa.edu.co>
2026-03-06 07:38:13 -0500

    tabla de multiplicar

--- taller_1044218091.py
+++ taller_1044218091.py
@@ -15,6 +15,33 @@ contador = 1
 while contador <= 5:
     print("numero: "  + str(contador))
     contador = contador + 1
+    
+  #while  
+for i in range (5): 
+    print("numero: " + str(i))
+    
+for i in range (1, 6): 
+    print("numero: " + str(i))
+    
+for i in range (0, 12, 3): 
+    print("numero: " + str(i))
+    
+  #bandera (flag)
+  
+encontrado = False
+numero = (1, 3, 5, 7, 9)
+numerobuscado = 7
+for numero in numero:
+    if numero == numerobuscado:
+        encontrado = True
+        break
+print("numero", numerobuscado, "encontrado:", str(encontrado))
+
+ #bucles anidados
+for i in range (3):
+    for j in range (3):
+        print("i:" + str(i) + "j:" + str (j))
+    
 
 
 
